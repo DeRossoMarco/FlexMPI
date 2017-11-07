fa93c2ce61cb74b2b8e7090517d8c825e7bd71d3
David Expósito Singh <dexposit@inf.uc3m.es>
2017-11-07 14:21:42 +0100

    New version

---
+++ scripts/Execute2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters"
+fi
+
+NODENAME=`uname -n`
+
+cp  $HOME/FlexMPI/configuration_files/nodefiles/nodefile.demo1 $HOME/FlexMPI/configuration_files/nodefiles/nodefile
+sed -i -e 's/nodename/'"$NODENAME"'/g' $HOME/FlexMPI/configuration_files/nodefiles/nodefile
+
+cp $HOME/FlexMPI/configuration_files/corefiles/corefile.demo1 $HOME/FlexMPI/configuration_files/corefiles/corefile
+sed -i -e 's/nodename/'"$NODENAME"'/g' $HOME/FlexMPI/configuration_files/corefiles/corefile
+
+sed -i -e 's/localhost/'"$NODENAME"'/g' $HOME/FlexMPI/controller/rankfiles/rankfile$4
+
+
+list=`cat $HOME/FlexMPI/configuration_files/nodefiles/nodefile |  awk -F: '{print $1}'`
+
+for item in $list; do
+  cp $HOME/FlexMPI/examples/jacobi_IO /tmp/jacobi_IO$4
+done
+
+
+$HOME/LIBS/mpich/bin/mpiexec -genvall -f $HOME/FlexMPI/controller/rankfiles/rankfile$4 -np $1 /tmp/jacobi_IO$4 500 10000 0.00001 100 1 70 -cfile $HOME/FlexMPI/configuration_files/corefiles/corefile -policy-malleability-triggered -lbpolicy-counts 15000 100 -ni 100 -ports $2 $3
+
+
