d81b1a4784cb8dd0c0f996c4a4614e9086bee4ba
David Expósito Singh <dexposit@inf.uc3m.es>
2018-10-31 13:39:47 +0100

    New version

--- scripts/Lanza_Jacobi_IO.sh
+++ scripts/Lanza_Jacobi_IO.sh
@@ -1,6 +1,9 @@
 nodefile=../run/nodefile1.dat
 MPIPATH=/usr/bin
 
+# Uncomment for a local installation of the libraries
+#MPIPATH=~/LIBS/mpich/bin/
+
 list=`cat  $nodefile |  awk -F: '{print $1}' `
 for item in $list; do
   echo Configuring node $item
