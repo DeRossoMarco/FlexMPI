5e10b7b0478a4db1c9deb3ad7dffaff0651dd95c
David Expósito Singh <dexposit@inf.uc3m.es>
2018-10-31 11:00:57 +0100

    New version

--- scripts/Lanza_Jacobi_IO.sh
+++ scripts/Lanza_Jacobi_IO.sh
@@ -5,13 +5,13 @@ list=`cat  $nodefile |  awk -F: '{print $1}' `
 for item in $list; do
   echo Configuring node $item
   rsh $item rm -f /tmp/jacobi_IO$4
-  rsh $item cp /home/desingh/FlexMPI/examples/jacobi_IO /tmp/jacobi_IO$4
+  rsh $item cp $HOME/FlexMPI/examples/jacobi_IO /tmp/jacobi_IO$4
 done
 
 controllernode=`cat ../controller/controller.dat`
 
-echo ~/LIBS/mpich/bin/mpiexec -genvall -f /home/desingh/FlexMPI/controller/rankfiles/rankfile$4 -np $1 /tmp/jacobi_IO$4 $5 $10 0.00001 $7 $8 $6 $4 $controllernode -cfile ../run/nodefile2.dat -policy-malleability-triggered -lbpolicy-static -ni 20 -ports $2 $3 -controller $controllernode -IOaction $9
+echo ~/LIBS/mpich/bin/mpiexec -genvall -f $HOME/FlexMPI/controller/rankfiles/rankfile$4 -np $1 /tmp/jacobi_IO$4 $5 $10 0.00001 $7 $8 $6 $4 $controllernode -cfile ../run/nodefile2.dat -policy-malleability-triggered -lbpolicy-static -ni 20 -ports $2 $3 -controller $controllernode -IOaction $9
 
-~/LIBS/mpich/bin/mpiexec -genvall -f /home/desingh/FlexMPI/controller/rankfiles/rankfile$4 -np $1 /tmp/jacobi_IO$4 $5 $10 0.00001 $7 $8 $6 $4 $controllernode -cfile ../run/nodefile2.dat -policy-malleability-triggered -lbpolicy-static -ni 20 -ports $2 $3 -controller $controllernode -IOaction $9
+~/LIBS/mpich/bin/mpiexec -genvall -f $HOME/FlexMPI/controller/rankfiles/rankfile$4 -np $1 /tmp/jacobi_IO$4 $5 $10 0.00001 $7 $8 $6 $4 $controllernode -cfile ../run/nodefile2.dat -policy-malleability-triggered -lbpolicy-static -ni 20 -ports $2 $3 -controller $controllernode -IOaction $9
 
 
